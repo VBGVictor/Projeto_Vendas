cc5d087a88136de140798fe7e2f743374f461071
Victor Barbosa Goveia <victor.barbosa.gov@gmail.com>
2022-05-17 18:59:46 -0300

    Projeto criado

---
+++ src/main/java/com/VictorBarbosa/Projeto_Vendas/ProjetoVendasApplication.java
@@ -0,0 +1,13 @@
+package com.VictorBarbosa.Projeto_Vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVendasApplication.class, args);
+	}
+
+}
